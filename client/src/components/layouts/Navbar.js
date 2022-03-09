43b17757247405d6879bee5cc8fa4923df6484bd
Sachin1612 <sachin.chouhan1612@gmail.com>
2022-03-09 22:25:20 +0530

    top navbar sticky

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -3,7 +3,7 @@ import { Link, NavLink } from "react-router-dom"
 
 export const Navbar = () => {
   return (
-    <div className="mx-auto p-5 shadow-md bg-white">
+    <div className="mx-auto p-5 shadow-md bg-white sticky top-0">
       <nav className="flex justify-between">
         <div className='rounded-full h-10 w-10 flex items-center justify-center bg-blue-600 text-xl font-extrabold text-white'>
           <Link to="/">lf</Link>
